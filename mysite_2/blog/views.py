5db0dce50fc9bb26e11b47a606de5d790b02c385
yssmiac3 <47303491+yssmiac3@users.noreply.github.com>
2020-06-16 20:46:12 +0300

    same posts added

--- mysite_2/blog/views.py
+++ mysite_2/blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 def post_share(request, post_id):
 	post = get_object_or_404(Post, id=post_id, status='published')
@@ -60,6 +61,7 @@ def post_detail(request, year, month, day, post):
                                    publish__day=day)
     comments = post.comments.filter(active=True)
     new_comment = None
+
     if request.method == 'POST':
     	comment_form = CommentForm(data=request.POST)
     	if comment_form.is_valid():
@@ -70,9 +72,16 @@ def post_detail(request, year, month, day, post):
     else:
     	comment_form = CommentForm()
 
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                    .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                    .order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/post/detail.html',
                   {'post': post,
                   'comments' : comments,
                   'new_comment' : new_comment,
-                  'comment_form' : comment_form})
+                  'comment_form' : comment_form,
+                  'similar_posts' : similar_posts,})
 
